0b5688be8edb66ab16c7ac748c7c93cbbc050d5f
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-07-07 14:47:39 +0300

    add locales

---
+++ src/js/locales/en.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    errors: {
+      ERR_NETWORK: 'Network error',
+      ERR_UNKNOWN: 'Unknown error. Something went wrong.',
+      ERR_ALREADY_EXISTS: 'RSS already exists',
+      ERR_INVALID_URL: 'The link must be a valid URL',
+      ERR_INVALID_RSS: 'The resource does not contain valid RSS',
+    },
+    feedbacks: {
+      uploaded: 'RSS uploaded successfully',
+    },
+  },
+};
---
+++ src/js/locales/ru.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    errors: {
+      ERR_NETWORK: 'Ошибка сети',
+      ERR_UNKNOWN: 'Неизвестная ошибка. Что-то пошло не так.',
+      ERR_ALREADY_EXISTS: 'RSS уже существует',
+      ERR_INVALID_URL: 'Ссылка должна быть валидным URL',
+      ERR_INVALID_RSS: 'Ресурс не содержит валидный RSS',
+    },
+    feedbacks: {
+      uploaded: 'RSS успешно загружен',
+    },
+  },
+};
