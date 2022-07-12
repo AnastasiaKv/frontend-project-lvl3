ee6118ddbd2d1c080bfe58a4277c1adb6dedc763
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-07-12 15:17:22 +0300

    add tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,17 +1,88 @@
-import { promises as fs } from 'fs';
+import '@testing-library/jest-dom';
+import { screen, waitFor } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+import fs from 'fs';
 import path from 'path';
-import app from '../src/js/application';
+import app from '../src/js/application.js';
+
+const pathToHtml = path.resolve(__dirname, '__fixtures__/index.html');
+const html = fs.readFileSync(pathToHtml, 'utf-8');
+const testRSSUrl = 'http://lorem-rss.herokuapp.com/feed?unit=second&interval=10';
+const htmlElements = {};
+let user;
 
 beforeEach(async () => {
-  const pathToHtml = path.resolve(__dirname, '__fixtures__/index.html');
-  const html = await fs.readFile(pathToHtml, 'utf8');
   document.body.innerHTML = html;
-});
-
-test('app', () => {
+  user = userEvent.setup();
   app();
-  expect(true).toBeDefined();
+
+  htmlElements.input = screen.getByRole('textbox', { name: 'url' });
+  htmlElements.submit = screen.getByRole('button', { name: 'add' });
 });
 
-// http://lorem-rss.herokuapp.com/feed,
-// http://lorem-rss.herokuapp.com/feed?unit=second&interval=30
+describe('Feedback validation', () => {
+  it('required', async () => {
+    await user.type(htmlElements.input, ' ');
+    await user.click(htmlElements.submit);
+    await waitFor(() => {
+      expect(screen.getByText('Не должно быть пустым')).toBeInTheDocument();
+    });
+  });
+
+  it('invalid url', async () => {
+    await user.type(htmlElements.input, 'test.com');
+    await user.click(htmlElements.submit);
+    await waitFor(() => {
+      expect(screen.getByText('Ссылка должна быть валидным URL')).toBeInTheDocument();
+    });
+  });
+
+  it('invalid rss', async () => {
+    await user.type(htmlElements.input, 'http://lorem-rss.herokuapp.com');
+    await user.click(htmlElements.submit);
+
+    await waitFor(() => {
+      expect(screen.getByText('Ресурс не содержит валидный RSS')).toBeInTheDocument();
+    });
+  });
+
+  it('uploaded', async () => {
+    await user.type(htmlElements.input, testRSSUrl);
+    await user.click(htmlElements.submit);
+    await waitFor(() => {
+      expect(screen.getByText('RSS успешно загружен')).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText('Фиды')).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText('Посты')).toBeInTheDocument();
+    });
+  });
+
+  it('uploaded & already exist', async () => {
+    await user.type(htmlElements.input, testRSSUrl);
+    await user.click(htmlElements.submit);
+
+    await waitFor(() => {
+      expect(screen.getByText('RSS успешно загружен')).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText('Фиды')).toBeInTheDocument();
+      const feeds = document.querySelectorAll('.feeds li');
+      expect(feeds).toHaveLength(1);
+    });
+    await waitFor(() => {
+      expect(screen.getByText('Посты')).toBeInTheDocument();
+      const posts = document.querySelectorAll('.posts li');
+      expect(posts).not.toHaveLength(0);
+    });
+
+    await user.type(htmlElements.input, testRSSUrl);
+    await user.click(htmlElements.submit);
+
+    await waitFor(() => {
+      expect(screen.getByText('RSS уже существует')).toBeInTheDocument();
+    });
+  });
+});
